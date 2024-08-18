7c113ad90ed58cd71cf83dcbf5e71bda2c22ea9b
Maksym Azimov <anthoniotheruiner@gmail.com>
2024-08-18 23:12:16 +0200

    feat: build improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/aba06b8e-2615-4f3b-99ae-e225f2eb3c0e-7wjbi6.jpg",
+  "https://utfs.io/f/da27a7d2-4eaa-433d-9c0f-629410e90cae-wl12qf.jpg",
+  "https://utfs.io/f/fd38c5d9-356f-44a9-95e5-368b7b466371-btfc9m.jpg",
+  "https://utfs.io/f/48efd5b4-c672-4103-a3b8-be716714ebb1-lkdgsa.jpg",
+];
+
+const mockImages = mockUrls.map((url, idx) => ({
+  id: idx + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello there!
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={`image-#${image.id}`} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
